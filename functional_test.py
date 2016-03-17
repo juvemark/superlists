1033f28a81e6a857e01713537aada63462ed234f
Qian Mao <juvemark@gmail.com>
2016-03-17 23:01:08 +0800

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 #browser = webdriver.Firefox()
@@ -17,18 +18,28 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8090')
 
 		# there is a 'TO-Do' in the title
-		assert 'To-Do' in self.browser.title
+		self.assertIn( 'To-Do', self.browser.title)
+		head_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn( 'To-Do', head_text)
 
 		# Application invide her to input an item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# Input "Buy peacock feather"
+		inputbox.send_keys('Buy peacock feathers')
 
 		# After Enter, page update
 		# Form show "1. Buy peacock feathers"
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows =  table.find_element_by_tag_name('tr')
+		self.assertTrue( any(row.text == '1: Buy peacock feathers' for row in rows))
 
 		# Show another dialog and it is possible to input another item
 		# She input "Use peacock feathers to make a fly"
-
+		self.fail('Finish the test!')
 		# Page update again, her list show two items
 
 		# She saw a URL, and some explanation about this URL
