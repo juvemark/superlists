97d47a5aaa88d96537b085d02e5b007b40d3ce46
unknown <eqiamao@CN00121433.ericsson.se>
2016-03-29 22:17:19 +0800

    first steps of FT for layout + styling

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -82,3 +82,23 @@ class NewVisitorTest(LiveServerTestCase):
 		# She visit the URL and found the list still exists
 
 		# End
+	def test_layout_and_styling(self):
+		#Edith visit the main page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+
+		#She finds input box show in the middle perfectly
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x']+inputbox.size['width']/2,
+			512,
+			delta=10
+		)
+
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x']+inputbox.size['width']/2,
+			512,
+			delta=10
+		)
