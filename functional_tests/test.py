aeda63a9f79b020bd5990958a177fe0924d98918
unknown <eqiamao@CN00121433.ericsson.se>
2016-03-30 16:07:35 +0800

    Hack FT runner to be able to test staging

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -5,8 +5,23 @@ from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import unittest
 import time
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -22,7 +37,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# input http://localhost:8090 to access webpage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		#time.sleep(10)
 
 		# there is a 'TO-Do' in the title
@@ -60,7 +75,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser=webdriver.Firefox()
 		# He visis the front page
 		# Cannot see the list of her
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -86,7 +101,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		# End
 	def test_layout_and_styling(self):
 		#Edith visit the main page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 
 		#She finds input box show in the middle perfectly
