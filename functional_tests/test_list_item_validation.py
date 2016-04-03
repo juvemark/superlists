0ea397b260ee3f6b762cc87fef75a8e7f23abe35
unknown <eqiamao@CN00121433.ericsson.se>
2016-04-03 16:18:57 +0800

    Move unit tests into a folder with single file

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
 from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 	@skip
-	def test_connot_add_empty_list_items(self):
+	def test_ca_innot_add_empty_list_items(self):
 		# Edith visit main page and input a empty item
+		self.browser.get(self.server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
 		# The main page refresh, and an error message shows
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# She input some words and submit again
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+		self.check_for_row_in_list_table('1: Buy milk')
 
 		# She submit an empty item again
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
 		# same error message shows
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# After input some words it works
-		self.fail("write me!")
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+		self.check_for_row_in_list_table('1: Buy milk')
+		self.check_for_row_in_list_table('2: Make tea')
