3023f6c3aa9b6f94517e156431fc686e1c82974a
unknown <eqiamao@CN00121433.ericsson.se>
2016-04-04 12:50:30 +0800

    rename all item input ids and names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,24 +9,24 @@ class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_empty_list_items(self):
 		# Edith visit main page and input a empty item
 		self.browser.get(self.server_url)
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 
 		# The main page refresh, and an error message shows
 		error = self.browser.find_element_by_css_selector('.has-error')
 		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# She input some words and submit again
-		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+		self.get_item_input_box().send_keys('Buy milk\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 
 		# She submit an empty item again
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 
 		# same error message shows
 		error = self.browser.find_element_by_css_selector('.has-error')
 		self.assertEqual(error.text, "You can't have an empty list item")
 
 		# After input some words it works
-		self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+		self.get_item_input_box().send_keys('Make tea\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 		self.check_for_row_in_list_table('2: Make tea')
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -16,7 +16,7 @@ class NewVisitorTest(FunctionalTest):
 		self.assertIn( 'To-Do', head_text)
 
 		# Application invide her to input an item
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# Input "Buy peacock feather"
@@ -25,14 +25,14 @@ class NewVisitorTest(FunctionalTest):
 		# After Enter, page update
 		# Form show "1. Buy peacock feathers"
 		inputbox.send_keys(Keys.ENTER)
-		edith_list_url = self.browser.cu
+		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# Show another dialog and it is possible to input another item
 		# She input "Use peacock feathers to make a fly"
 
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
@@ -51,7 +51,7 @@ class NewVisitorTest(FunctionalTest):
 		self.assertNotIn('make a fly', page_text)
 
 		#He input a new item and create a list
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 
