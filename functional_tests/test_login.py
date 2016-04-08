8839f212c5bb49c6b19578fd358a71e4bff9059d
unknown <eqiamao@CN00121433.ericsson.se>
2016-04-08 18:21:13 +0800

    FT for login with Persona

---
+++ functional_tests/test_login.py
@@ -0,0 +1,42 @@
+from .base import FunctionalTest
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+
+class LoginTest(FunctionalTest):
+
+    def test_login_with_persona(self):
+        # Edith visit this super good website
+        # There is a sign in link
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('login').click()
+
+        # Persona dialog shows
+        self.switch_to_new_window('Mozilla Persona')
+
+        #Edith use her email to log in
+        self.browser.find_element_by_id('authentication_email').send_keys('edith@mockmyid.com')
+        self.browser.find_element_by_tag_name('button').click()
+
+        #Persona dialog close
+        self.switch_to_new_window('To-Do')
+
+        #She found she has already log in
+        self.wait_for_element_with_id('logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('edith@mockmyid.com', navbar.text)
+
+    def switch_to_new_window(self, text_in_title):
+        retries = 60
+        while retries>0:
+            for handle in self.browser.window_handles:
+                self.browser.switch_to_window(handle)
+                if text_in_title in self.browser.title:
+                    return
+            retries -= 1
+            time.sleep(0.5)
+        self.fail('could not find window')
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id)
+        )
