be4c130aead0871f46a5cf2d29a2606745f7c0e7
Qian Mao <juvemark@gmail.com>
2016-04-22 15:38:46 +0800

    placeholder test_my_lists and move login checkers into base

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,7 @@
 from .base import FunctionalTest
 import time
 from selenium.webdriver.support.ui import WebDriverWait
+TEST_EMAIL = 'edith@mockmyid.com'
 
 class LoginTest(FunctionalTest):
 
@@ -21,19 +22,19 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         #She found she has already log in
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email='edith@mockmyid.com')
 
         #refresh and she dfind it is real
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email='edith@mockmyid.com')
 
         #She click log out
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email='edith@mockmyid.com')
 
         # refresh still log out
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email='edith@mockmyid.com')
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
@@ -46,20 +47,3 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail('could not find window')
 
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}. Page text was {}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
-
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('edith@mockmyid.com', navbar.text)
